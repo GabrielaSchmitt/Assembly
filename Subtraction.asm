fe5782bf9880a6e3e8c3ef94b65048ce27982d2f
Gabriela Schmitt <86369677+GabrielaSchmitt@users.noreply.github.com>
2022-12-03 01:12:58 -0300

    Create Subtraction.asm

---
+++ Subtraction.asm
@@ -0,0 +1,28 @@
+# EN-US Translate the following algorithm in C language to Assembly language
+# PT-BR Traduza o seguinte algoritmo na linguagem C para a linguagem Assembly
+#
+# int a, b, c;
+# a = 3;
+# b = 5;
+# c = a - b;
+
+.data 
+a: .word 3 # int a = 3
+b: .word 5 # int b = 5
+c: .word 
+
+.text
+# load a // carrega a
+la $8, a
+lw $8, ($8)
+
+# load b // carrega b
+la $9, b
+lw $9, ($9)
+
+# subtract a - b and allocate in register $10 // subtrai 3 - 5 e aloca no registrador $10
+subu $10, $8, $
+
+# Store the result of the subtraction // Salva o resultado da subtração em c 
+la $11, c
+sw $10, ($11)
